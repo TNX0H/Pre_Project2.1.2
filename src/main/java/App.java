1ff8e77544b8d15c35914185f72fbfd4a4922731
TNX0H <dimanmityan@yandex.ru>
2022-07-28 22:45:45 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,15 +6,15 @@ public class App {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
         HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
 
-        Cat bean2 = (Cat) applicationContext.getBean("cat");
-        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
 
-        System.out.println(bean.equals(bean1));
-        System.out.println(bean2.equals(bean3));
+        System.out.println(bean1 == bean2);
+        System.out.println(cat1 == cat2);
     }
 }
